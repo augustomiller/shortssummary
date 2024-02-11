f9312986cbb905030cdda26003f02ab42b7dc46e
Miller <maicmiller42@gmail.com>
2024-02-11 16:09:39 -0300

    feat: add video download  method.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import cors from 'cors'
 import express, { request, response } from 'express'
 
+import { download } from './download.js'
+
 const app = express()
 app.use(cors())
 
 app.get("/summary/:id", (request, response) => {
-    response.send("ID do vídeo: " + request.params.id)
+    download(request.params.id)
+
+    response.json({ result: "Download do vídeo realizado com sucesso!"})
 })
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
